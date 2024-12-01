b7e76be9266201566dd3a1e39b9bd30780b1b0bc
Andre Flores <floresandre91@gmail.com>
2024-12-01 13:52:22 -0800

    Added notes about making maps

--- src/wall.cpp
+++ src/wall.cpp
@@ -8,6 +8,12 @@ Rectangle TOP_RIGHT_CORNER =    { 0, 0, 0, 0 };
 Rectangle BOTTOM_RIGHT_CORNER = { 0, 0, 0, 0 };
 Rectangle BOTTOM_LEFT_CORNER =  { 0, 0, 0, 0 };
 
+/*What if I used 16x16 blocks for the walls, assigned each vertical or 
+horizontalwall a different value like 1 and 2 repsectively, then used a 
+CSV sheet in excel to create the map?
+could make making the map way easier, give ability to change up the 
+maps in the future*/
+
 
 Wall::Wall(){
     float x = 100;
